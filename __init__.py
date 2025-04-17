bc4c40336251b7b34ad16cb9df0572e3762fe000
k_line <72039683+Keliane002@users.noreply.github.com>
2025-04-17 11:44:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,6 +26,21 @@ def meteo():
             temp_celsius = temp_kelvin - 273.15
             results.append({'Jour': dt_value, 'temp': round(temp_celsius, 2)})
     return jsonify(results=results)
+
+@app.route('/tawarano_html/')
+def meteo_html():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_kelvin = list_element.get('main', {}).get('temp')
+        if temp_kelvin is not None:
+            temp_celsius = temp_kelvin - 273.15
+            results.append({'Jour': dt_value, 'temp': round(temp_celsius, 2)})
+    return render_template("meteo.html", meteo=results)
+
                                                                                                                                       
 @app.route('/')
 def hello_world():
